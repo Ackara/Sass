18fe69f6577eac95ed2321aaa72eb23f91c0bba1
Ackara <ackara.dev@outlook.com>
2019-10-24 21:46:52 -0400

    Add main projects

---
+++ src/Sass/NodeEngine.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Acklann.Sass
+{
+	public class NodeEngine
+	{
+		
+		
+		#region Backing Members
+		#endregion
+	}
+}
---
+++ tests/Sass.MSTest/Tests/CompilationTest.cs
@@ -0,0 +1,19 @@
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Acklann.Sass.Tests
+{
+	[TestClass]
+	public class CompilationTest
+	{
+		
+	}
+}
