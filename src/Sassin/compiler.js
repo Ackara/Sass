00c27834a06c9397b0d93fb753fc0471ed8d4713
Ackara <ackara.dev@outlook.com>
2019-11-04 22:56:11 -0400

    Update compiler.js and index.html

--- src/Sassin/compiler.js
+++ src/Sassin/compiler.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const csso = require("csso");
 const sass = require("node-sass");
-const mergeSourceMap = require("multi-stage-sourcemap").transfer;
+const sourceMapMerger = require("multi-stage-sourcemap").transfer;
 
 // Methods
 // ==================================================
@@ -37,14 +37,20 @@ function compile(args) {
         }
 
         if (args.minify) {
-            let minifiedResult = minify(cssFile, sassResult.css.toString(), args);
+            let minifiedResult = minify(path.relative(args.sourceMapDirectory, cssFile), sassResult.css.toString(), args);
             let minifiedCss = minifiedResult.css.toString();
             let minifiedFile = args.getOutputPath();
 
             if (args.generateSourceMaps) {
-                let finalMap = args.getSourceMapPath();
-                minifiedCss = (minifiedCss + os.EOL + "/*# sourceMappingURL=" + path.relative(args.outputDirectory, finalMap) + " */");
-                mergeSourceMap(minifiedResult.map.toString(), sassResult.map.toString(), path.relative(args.sourceMapDirectory, minifiedFile));
+                let finalMapFile = args.getSourceMapPath();
+                let mergedMap = mergeSourceMaps(
+                    minifiedResult.map.toString(),
+                    sassResult.map.toString()
+                );
+                createFile(finalMapFile, JSON.stringify(mergedMap, null, 2));
+                generatedFiles.push(finalMapFile);
+
+                minifiedCss = (minifiedCss + os.EOL + "/*# sourceMappingURL=" + path.relative(args.outputDirectory, finalMapFile) + " */");
             }
 
             createFile(minifiedFile, minifiedCss, args);
@@ -64,11 +70,16 @@ function compile(args) {
     });
 }
 
-function minify(cssFile, css, args) {
-    return csso.minify(css, {
+function minify(cssFile, content, args) {
+    return csso.minify(content, {
         filename: cssFile,
         sourceMap: args.generateSourceMaps
     });
+}
+
+function mergeSourceMaps(mapB, mapA) {
+    var result = sourceMapMerger({ fromSourceMap: mapB, toSourceMap: mapA });
+    return JSON.parse(result.toString());
 }
 
 function createFile(absoluePath, content) {
